e57c598e48f78c2a3e3ade59ea053c892f57d43f
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-08-30 02:33:27 +0700

    Add Activity resource with CRUD functionality and schema configuration

---
+++ app/Filament/Resources/Activities/Schemas/ActivityForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Activities\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ActivityForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                FileUpload::make('image')
+                    ->image(),
+                DatePicker::make('date')
+                    ->required(),
+            ]);
+    }
+}
